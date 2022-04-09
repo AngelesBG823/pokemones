fb2bb544f021dadb3b4fb8e8e3bbe4f877673056
AngelesBG823 <angelesbarria823@gmail.com>
2022-04-09 13:11:45 -0400

    finished: pokemones.sql

--- pokemones.sql
+++ pokemones.sql
@@ -14,3 +14,11 @@ SELECT * FROM pokemones;
 
 \copy pokemones FROM 'pokemones.csv' csv header;
 SELECT * FROM pokemones;
+
+\copy pokemones FROM 'pokemones.cqv' csv header;
+SELECT * FROM pokemones;
+SELECT name AS nombres, pokedex, AS numero FROM pokemones;
+SELECT pokedex, name FROM pokemones WHERE name = 'Squirtle';
+SELECT * FROM pokemones WHERE type1 = 'Fire';
+SELECT * FROM pokemones WHERE type1 = 'Normal' AND type2 = 'Flying';
+SELECT * FROM pokemones WHERE type1 = 'Normal'
